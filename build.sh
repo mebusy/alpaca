e68cf6ccfcaceaac6eac2675fa9187eb73078bd2
mebusy <golden_slime@hotmail.com>
2026-07-03 10:24:21 +0800

    m

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ set -e
 
 go build -ldflags  "-w "  -o alpaca  $(ls -1 *.go | grep -v _test.go | grep -v "\(_unix\|_windows\|keyring\).go") 
 
-mv alpaca /usr/local/opt/alpaca/bin/alpaca
+mv alpaca `brew --prefix alpaca`/bin/alpaca
